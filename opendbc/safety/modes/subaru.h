608be7e9d0eb9b13d198f491a23cc6857afcdf8b
ClockeNessMnstr <locke.dftc@gmail.com>
2025-05-07 20:44:12 -0400

    Forester Slew Rate

--- opendbc/safety/modes/subaru.h
+++ opendbc/safety/modes/subaru.h
@@ -134,7 +134,7 @@ static void subaru_rx_hook(const CANPacket_t *msg) {
 }
 
 static bool subaru_tx_hook(const CANPacket_t *msg) {
-  const TorqueSteeringLimits SUBARU_STEERING_LIMITS      = SUBARU_STEERING_LIMITS_GENERATOR(2047, 50, 70);
+  const TorqueSteeringLimits SUBARU_STEERING_LIMITS      = SUBARU_STEERING_LIMITS_GENERATOR(2047, 30, 30);
   const TorqueSteeringLimits SUBARU_GEN2_STEERING_LIMITS = SUBARU_STEERING_LIMITS_GENERATOR(1000, 40, 40);
   const TorqueSteeringLimits SUBARU_IMPREZA_2018_STEERING_LIMITS = SUBARU_STEERING_LIMITS_GENERATOR(3071, 60, 60);
 
