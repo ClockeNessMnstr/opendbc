a38a63a5055f4f9e10c2e873e368aa4c7e0fb84b
ClockeNessMnstr <locke.dftc@gmail.com>
2025-05-07 20:41:40 -0400

    3071 Impreza / Crosstrek

--- opendbc/safety/tests/common.py
+++ opendbc/safety/tests/common.py
@@ -881,6 +881,8 @@ class PandaSafetyTest(PandaSafetyTestBase):
               continue
             if attr.startswith('TestSubaruGen') and current_test.startswith('TestSubaruGen'):
               continue
+            if attr.startswith('TestSubaruImp') and current_test.startswith('TestSubaruImp'):
+              continue
             if attr.startswith('TestSubaruPreglobal') and current_test.startswith('TestSubaruPreglobal'):
               continue
             if {attr, current_test}.issubset({'TestVolkswagenPqSafety', 'TestVolkswagenPqStockSafety', 'TestVolkswagenPqLongSafety'}):
